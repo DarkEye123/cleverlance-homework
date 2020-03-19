ec24053c4f89752f47d1f40807152e1a02d1825d
DarkEye <lesko.matej.pu@gmail.com>
2020-03-20 00:53:58 +0100

    alert component

---
+++ src/components/Alert/index.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { Alert, AlertTitle } from '@material-ui/lab';
+import { Close as CloseIcon } from '@material-ui/icons';
+import { Zoom, Fade, IconButton } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
+import { ApolloError } from '@apollo/client';
+import { motion } from 'framer-motion';
+
+type Message = { message?: string; id: number };
+interface CloseHandler {
+  onClose: (id: number) => void;
+}
+interface ResultShape {
+  result?: {
+    errors: Message[];
+  };
+}
+
+type NetworkError = {
+  networkError?: (ResultShape & Error) | null;
+} & ApolloError;
+
+const messagesWithIDs = (list: Message[]): Message[] =>
+  list?.map(({ message }, i) => ({ message, id: i }));
+
+const ErrorAlert = ({ message, id, onClose }: Message & CloseHandler) => {
+  const { t } = useTranslation();
+  const [visible, setVisible] = useState(true);
+  const [isClosed, setIsClosed] = useState(false);
+  return (
+    (!isClosed && message && (
+      <Fade
+        in={visible}
+        timeout={500}
+        onExited={() => {
+          onClose(id);
+          setIsClosed(true);
+        }}
+      >
+        <div>
+          <Zoom timeout={500} in={visible}>
+            <Alert
+              onClick={() => {
+                setVisible(false);
+              }}
+              action={
+                <IconButton
+                  aria-label="close"
+                  color="inherit"
+                  size="small"
+                  onClick={() => {
+                    setVisible(false);
+                  }}
+                >
+                  <CloseIcon />
+                </IconButton>
+              }
+              severity="error"
+              data-testid="graphql-error"
+            >
+              <AlertTitle>{t('problem')}!</AlertTitle>
+              {message}
+            </Alert>
+          </Zoom>
+        </div>
+      </Fade>
+    )) ||
+    null
+  );
+};
+
+export default ({ error }: { error: NetworkError | undefined }) => {
+  if (!error) return null;
+  const [errors, setErrors] = useState(
+    messagesWithIDs(
+      (error?.networkError?.result?.errors || [error]) as Message[],
+    ),
+  );
+  let items = errors.map(({ message, id }) => (
+    <motion.div positionTransition key={id}>
+      {console.log(error)}
+      <ErrorAlert
+        message={message}
+        id={id}
+        onClose={(id: number) => setErrors(errors.filter(e => e.id != id))}
+      />
+    </motion.div>
+  ));
+  return <>{items}</>;
+};
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as Pupils } from './Pupils';
 export { default as Subjects } from './Subjects';
+export { default as Alert } from './Alert';
