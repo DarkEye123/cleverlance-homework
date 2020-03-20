e27ccaf8b1f4ee2957f58f258aceaa1ca70ec405
DarkEye <lesko.matej.pu@gmail.com>
2020-03-20 01:54:12 +0100

    container used for wrapping students

--- src/components/Students/index.tsx
+++ src/components/Students/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Backdrop, CircularProgress, Grid } from '@material-ui/core';
+import {
+  Backdrop,
+  CircularProgress,
+  Grid,
+  Container,
+  Box,
+} from '@material-ui/core';
 import { useQuery } from '@apollo/client';
 import { STUDENTS_QUERY } from '../../resolvers';
 import { Alert } from '../index';
@@ -7,7 +13,7 @@ import { Alert } from '../index';
 const Students = () => {
   const { loading, error, data } = useQuery(STUDENTS_QUERY);
   return (
-    <>
+    <Box component={Container} mt={8}>
       {data && (
         <Grid container direction="column" spacing={1}>
           {data.allStudents.map((p: any) => (
@@ -19,7 +25,7 @@ const Students = () => {
         <CircularProgress />
       </Backdrop>
       <Alert error={error} />
-    </>
+    </Box>
   );
 };
 
