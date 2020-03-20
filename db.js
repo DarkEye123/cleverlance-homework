34fe79182be56990ba736f68b4e958dadc40b823
DarkEye <lesko.matej.pu@gmail.com>
2020-03-20 23:06:53 +0100

    db update

--- db.js
+++ db.js
@@ -19,12 +19,44 @@ module.exports = {
   subjects: [
     { id: 1, name: 'Mathematics' },
     { id: 2, name: 'Philosophy' },
+    { id: 3, name: 'Astronomy' },
   ],
   studentSubjects: [
     {
       id: 1,
       student_id: 1,
       subject_id: 1,
+      score: 3,
+    },
+    {
+      id: 2,
+      student_id: 1,
+      subject_id: 2,
+      score: 2,
+    },
+    {
+      id: 3,
+      student_id: 1,
+      subject_id: 3,
+      score: 4,
+    },
+    {
+      id: 4,
+      student_id: 2,
+      subject_id: 1,
+      score: 1,
+    },
+    {
+      id: 5,
+      student_id: 2,
+      subject_id: 2,
+      score: 1,
+    },
+    {
+      id: 6,
+      student_id: 2,
+      subject_id: 3,
+      score: 3,
     },
   ],
 };
