b20454ada986bd7bf89a094125f8b07174ed2bc5
DarkEye <lesko.matej.pu@gmail.com>
2020-03-20 23:09:07 +0100

    not found page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,25 @@
 import React, { FC } from 'react';
-import { Route } from 'react-router-dom';
-import { Students, Subjects } from './views';
+import { Route, Switch } from 'react-router-dom';
+import { Students, Subjects, NotFound, Book } from './views';
 
 const PAGES = {
   home: '/',
   students: '/students',
   subjects: '/subjects',
+  book: '/book/:id',
   help: '/help',
 };
 const Routes: FC = () => (
-  <>
+  <Switch>
     <Route path={PAGES.students} exact component={Students} />
-    <Route path={PAGES.subjects} component={Subjects} />
+    <Route path={PAGES.subjects} exact component={Subjects} />
+    <Route path={PAGES.book} component={Book} />
     {/* <Route path={PAGES.help} component={Help} /> */}
     <Route path={PAGES.home} exact component={Students} />
-  </>
+    <Route path="*">
+      <NotFound />
+    </Route>
+  </Switch>
 );
 
 export { PAGES };
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,2 +1,3 @@
 export { default as Students } from './Students';
 export { default as Subjects } from './Subjects';
+export { default as NotFound } from './NotFound';
